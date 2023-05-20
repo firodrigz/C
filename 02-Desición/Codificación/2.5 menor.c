ca36f7a3ad4757e79e774f1f558488a4e7015290
Facundo Rodriguez <firodrigz@gmail.com>
2023-05-19 21:35:28 -0300

    Add files via upload

---
+++ 02-Desición/Codificación/2.5 menor.c
@@ -0,0 +1,4 @@
+/*Confeccionar un programa que permita ingresar 4 números enteros, de a uno por vez y determine el menor
+valor y su número de orden. Informe los valores ingresados e identifique al menor con mensaje aclaratorio*/
+
+#include <stdio.h>
