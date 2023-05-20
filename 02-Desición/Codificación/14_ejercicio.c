c716196db96eca2f870d007e9b173da3ae31cfc2
Facundo Rodriguez <firodrigz@gmail.com>
2023-05-20 17:46:48 -0300

    Desición

---
+++ 02-Desición/Codificación/14_ejercicio.c
@@ -0,0 +1,8 @@
+/*Un negocio vende distintos artículos identificados por un código, según se muestra:
+- código 1; 10; 100: 10 pesos la unidad.
+- código 2; 22; 222: 7 pesos la unidad. La caja de 10 unidades vale 65 pesos.
+- código 3; 33: 3 pesos la unidad. Si la compra es por más de 10 unidades se hace un descuento del
+10% sobre el total de la compra.
+- código 4; 44: 1 peso la unidad.
+Confeccionar un programa que ingrese como dato el código de un artículo y la cantidad a comprar y se
+informe el importe de la compra*/
