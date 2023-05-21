7d497e549441b733989181b6efd8da340cd975c0
Facundo Rodriguez <firodrigz@gmail.com>
2023-05-21 16:10:57 -0300

    Estructura Iteración Definada - concepto

---
+++ 03- Iteración Definida/Teoría/maximo_minimo_FOR_PrimeraForma.c
@@ -0,0 +1,40 @@
+/**********************************************************************
+En un laboratorio se realizan ensayos. Por cada ensayo se registra:
+// Nro. del ensayo y temperatura medida.
+// Hay 5 ensayos
+// Determinar:
+// a-) la temperatura maxima medida
+// b-) Nro. de ensayo de temperatura minima
+// NOTA: TODOS LOS VALORS DE TEMPERATURA SON DISTINTOS
+***********************************************************************/
+#include <stdio.h>
+main()
+{
+  int NE, TEMP, TMAX, TMIN, NEMIN, CONT;
+
+  for (CONT = 1 ; CONT <= 5 ; CONT ++)
+  {
+  	printf ("\n INGRESAR NRO. de ENSAYO ");
+    scanf ("%d",&NE);
+	printf ("\n INGRESAR TEMPERATURA ");
+    scanf ("%d",&TEMP);
+
+    if (CONT == 1)
+     {
+       TMAX = TEMP;
+	   TMIN = TEMP;
+	   NEMIN = NE;
+      }
+    else
+       if (TEMP > TMAX)
+           TMAX = TEMP;
+       else
+	      if (TEMP < TMIN)
+	         {
+	           TMIN = TEMP;
+			   NEMIN = NE;
+	         }
+  }
+   printf ("\n LA TEMPERATURA MAXIMA ES %d", TMAX);
+   printf ("\n EL NRO. DE ENSAYO DONDE SE PRODUJO LA TEMPERATURA MINIMA ES %d", NEMIN);
+}
