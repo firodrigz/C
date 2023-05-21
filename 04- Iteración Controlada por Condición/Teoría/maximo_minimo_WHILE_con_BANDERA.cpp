0924c85ead4bbe60fa67b394b1d4027675d20dad
Facundo Rodriguez <firodrigz@gmail.com>
2023-05-21 15:46:00 -0300

    Estructura Iteración Definada - concepto

---
+++ 04- Iteración Controlada por Condición/Teoría/maximo_minimo_WHILE_con_BANDERA.cpp
@@ -0,0 +1,49 @@
+/****************************************************************************
+En un laboratorio se realizan ensayos. Por cada ensayo se registra:
+// Nro. del ensayo y temperatura medida.
+// LA INFORMACION TERMINA CON NRO. DE ENSAYO NEGATIVO O CERO
+// Determinar:
+// a-) la temperatura maxima medida
+// b-) Nro. de ensayo de temperatura minima
+// NOTA: TODOS LOS VALORS DE TEMPERATURA SON DISTINTOS
+/***************************************************************************/
+#include <stdio.h>
+main()
+{
+  int NE, TEMP, TMAX, TMIN, NEMIN, BANDERA=0;
+
+  printf ("\n INGRESAR NRO. de ENSAYO (TERMINA CON NEGATIVO O CERO) ");
+  scanf ("%d",&NE);
+  while (NE > 0)
+  {
+  	printf ("\n INGRESAR TEMPERATURA ");
+    scanf ("%d",&TEMP);
+
+    if (BANDERA == 0)
+     {
+       TMAX = TEMP;
+	   TMIN = TEMP;
+	   NEMIN = NE;
+	   BANDERA = 1;
+     }
+    else
+       if (TEMP > TMAX)
+           TMAX = TEMP;
+       else
+	      if (TEMP < TMIN)
+	         {
+	           TMIN = TEMP;
+			   NEMIN = NE;
+	         }
+
+  printf ("\n INGRESAR NRO. de ENSAYO (TERMINA CON NEGATIVO O CERO)");
+  scanf ("%d",&NE);
+  }
+   if (BANDERA == 1)
+   {
+    printf ("\n LA TEMPERATURA MAXIMA ES %d", TMAX);
+    printf ("\n EL NRO. DE ENSAYO DONDE SE PRODUJO LA TEMPERATURA MINIMA ES %d", NEMIN);
+   }
+   else
+    printf("\n NO SE INGRESARON ENSAYOS");
+}
