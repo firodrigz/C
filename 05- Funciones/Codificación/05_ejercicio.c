23c78c33df8dc7a328cb14570e9d17860bdb8f40
firodrigz <firodrigz@gmail.com>
2023-06-08 19:44:57 -0300

    Menú según ternas

--- 05- Funciones/Codificación/05_ejercicio.c
+++ 05- Funciones/Codificación/05_ejercicio.c
@@ -14,45 +14,70 @@ la condición final.*/
 #include <stdio.h>
 #include <locale.h>
 
+//Prototipo de funciones
+
 int validarIngreso(int,int);
-void mostrarMenu();
+int mostrarMenu();
 float ingreso();
 int valoresOrdenados(float,float,float);
+float mayorValor(float,float,float);
+float suma(float,float,float);
+float promedio(float,float,float);
 
+
+/*-----------------------------------------------------------------------*/
 
 int main(){
 	
-	float valor1, valor2, valor3;
-	int ternas = 1;  //preguntar
+	float valor1, valor2, valor3, valorMostrar;
+	int ternas = 1; 
+	int opcion;
 	
 	
 	setlocale(LC_ALL,"spanish");
 	
-	printf("\nTerna número: %d \n"),ternas;
+	printf("\nTerna número: %d \n",ternas);
 	valor1= ingreso();
 	valor2= ingreso();
-	valor3= ingreso();
+	valor3= ingreso();	
 	
-	mostrarMenu();
 	
 	while(valoresOrdenados(valor1,valor2,valor3)!=1){
-				
 		
-		printf("\nTerna número: %d \n"),ternas;
-		valor1= ingreso(ternas);
-		valor2= ingreso(ternas);
-		valor3= ingreso(ternas);
+		opcion=mostrarMenu();			
 		
 		ternas++;
 		
-		mostrarMenu();
+		while(opcion!=4){	
+			
+			switch(opcion){
+				case 1: valorMostrar = mayorValor(valor1,valor2,valor3);
+						printf("\nEl mayor valor es: %.2f", valorMostrar);
+				break;
+				 
+				case 2: valorMostrar = promedio(valor1,valor2,valor3);
+						printf("\nEl promedio de los números ingresados es: %.2f", valorMostrar);
+				break;
+				
+				case 3: valorMostrar = suma(valor1,valor2,valor3);
+						printf("\nLa suma de los números ingresados es: %.2f", valorMostrar);
+				break;						
+			}
+			
+			opcion=mostrarMenu();			
+		}
+			
+		printf("\nTerna número: %d \n",ternas);
+		valor1= ingreso();
+		valor2= ingreso();
+		valor3= ingreso();	
+						
 	}
-	
-	
+		
 	return 0;
 }
 
-
+//Definición de funciones
 
 int validarIngreso(int LI,int LS){
 	int band=0;
@@ -73,9 +98,13 @@ int validarIngreso(int LI,int LS){
 	return nro;
 }
 
-void mostrarMenu(){
+int mostrarMenu(){
+	int opcion;
+	
 	printf("\n\n1-Mayor valor \n2-Promedio \n3-Suma \n4-Finalizar \n\n");
-	validarIngreso(1,4);
+	opcion=validarIngreso(1,4);
+	
+	return opcion;
 }
 
 float ingreso(){
@@ -96,3 +125,33 @@ int valoresOrdenados(float a, float b, float c){
 		return 0;
 	}
 }
+
+float mayorValor(float a,float b,float c){
+	float mayor;
+		
+	mayor=a;
+	
+	if(b>mayor){
+		mayor=b;
+	}
+	else{
+		if(c>mayor){
+			mayor=c;
+		}
+	}	
+	
+	return mayor;
+}
+
+
+float suma(float a,float b,float c){
+	return (a+b+c);	
+}
+
+float promedio(float a,float b,float c){
+	float promedio;
+	
+	promedio= suma(a,b,c)/3;
+		
+}
+
