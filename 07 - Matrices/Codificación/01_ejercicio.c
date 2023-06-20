d166decd9a2d09adaaaef75b64b90c97f4fe0282
firodrigz <firodrigz@gmail.com>
2023-06-19 23:28:20 -0300

    Producto de matriz por constante

---
+++ 07 - Matrices/Codificación/01_ejercicio.c
@@ -0,0 +1,62 @@
+/*Ingresar una matriz de 3x3 por teclado y un número entero. Realizar el producto de la matriz por la
+constante.*/
+
+#include <stdio.h>
+#define FILAS 3
+#define COLUMNAS 3
+
+void ingresarMatriz(int [][COLUMNAS]);
+void multiplicarMatrizPorConstante(int [][COLUMNAS], int);
+void imprimirMatriz(int [][COLUMNAS]);
+
+int main() {
+    int matriz[FILAS][COLUMNAS];
+    int constante;
+    
+    ingresarMatriz(matriz);
+    
+    printf("Ingrese un número entero para multiplicar la matriz: ");
+    scanf("%d", &constante);
+    
+    multiplicarMatrizPorConstante(matriz, constante);
+    
+    imprimirMatriz(matriz);
+    
+    return 0;
+}
+
+void ingresarMatriz(int matriz[FILAS][COLUMNAS]) {
+    int i, j;
+    
+    printf("Ingrese los elementos de la matriz:\n");
+    
+    for (i = 0; i < FILAS; i++) {
+        for (j = 0; j < COLUMNAS; j++) {
+            printf("Ingrese el elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+
+void multiplicarMatrizPorConstante(int matriz[FILAS][COLUMNAS], int constante) {
+    int i, j;
+    
+    for (i = 0; i < FILAS; i++) {
+        for (j = 0; j < COLUMNAS; j++) {
+            matriz[i][j] *= constante;
+        }
+    }
+}
+
+void imprimirMatriz(int matriz[FILAS][COLUMNAS]) {
+    int i, j;
+    
+    printf("La matriz resultante es:\n");
+    
+    for (i = 0; i < FILAS; i++) {
+        for (j = 0; j < COLUMNAS; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
