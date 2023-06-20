fb185e6060280c8ddec776bf0894a5f55247fee0
firodrigz <firodrigz@gmail.com>
2023-06-20 01:31:09 -0300

    Producto de matriz por constante - parámetros

--- 07 - Matrices/Codificación/01_ejercicio.c
+++ 07 - Matrices/Codificación/01_ejercicio.c
@@ -5,54 +5,54 @@ constante.*/
 #define FILAS 3
 #define COLUMNAS 3
 
-void ingresarMatriz(int [][COLUMNAS]);
-void multiplicarMatrizPorConstante(int [][COLUMNAS]);
-void imprimirMatriz(int [][COLUMNAS]);
+void ingresarMatriz(int [][COLUMNAS], int, int);
+void multiplicarMatrizPorConstante(int [][COLUMNAS], int, int);
+void imprimirMatriz(int [][COLUMNAS], int, int);
 
 int main() {
     int matriz[FILAS][COLUMNAS];
         
-    ingresarMatriz(matriz);
-    multiplicarMatrizPorConstante(matriz);
-    imprimirMatriz(matriz);
+    ingresarMatriz(matriz,FILAS,COLUMNAS);
+    multiplicarMatrizPorConstante(matriz,FILAS,COLUMNAS);
+    imprimirMatriz(matriz,FILAS,COLUMNAS);
     
     return 0;
 }
 
-void ingresarMatriz(int matriz[][COLUMNAS]) {
+void ingresarMatriz(int matriz[][COLUMNAS],int cf, int cc) {
     int i, j;
     
     printf("Ingrese los elementos de la matriz:\n");
     
-    for (i = 0; i < FILAS; i++) {
-        for (j = 0; j < COLUMNAS; j++) {
+    for (i = 0; i < cf; i++) {
+        for (j = 0; j < cc; j++) {
             printf("Ingrese el elemento [%d][%d]: ", i, j);
             scanf("%d", &matriz[i][j]);
         }
     }
 }
 
-void multiplicarMatrizPorConstante(int matriz[][COLUMNAS]) {
+void multiplicarMatrizPorConstante(int matriz[][COLUMNAS],int cf, int cc) {
     int i, j;
     int constante;
     
     printf("Ingrese un número entero para multiplicar la matriz: ");
     scanf("%d", &constante);    
     
-    for (i = 0; i < FILAS; i++) {
-        for (j = 0; j < COLUMNAS; j++) {
+    for (i = 0; i < cf; i++) {
+        for (j = 0; j < cc; j++) {
             matriz[i][j] *= constante;
         }
     }
 }
 
-void imprimirMatriz(int matriz[][COLUMNAS]) {
+void imprimirMatriz(int matriz[][COLUMNAS],int cf, int cc) {
     int i, j;
     
     printf("La matriz resultante es:\n");
     
-    for (i = 0; i < FILAS; i++) {
-        for (j = 0; j < COLUMNAS; j++) {
+    for (i = 0; i < cf; i++) {
+        for (j = 0; j < cc; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
