81d1f8935cea15b0a7d1d5b6a8bce14fd401b6f0
firodrigz <firodrigz@gmail.com>
2024-04-01 05:13:01 -0300

    Aplicación de punteros en funciones

--- 13 - Punteros/Teoría/conceptosBasicos.c
+++ 13 - Punteros/Teoría/conceptosBasicos.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+struct empleados {
+	char nombre[10];
+	char apellido[15];
+	int edad;
+	float sueldo;
+};
+
+void muestraDatos(struct empleados *emp);
+
 int main(){
 
     int nro, *punt_nro;
@@ -12,6 +21,30 @@ int main(){
 
     printf("\n[+] El nÃºmero ingresado es: %d", *punt_nro); //Se pone * para ingresar al contenido de la direcciÃ³n de memoria apuntada.
     printf("\n[+] Almacenado en la direcciÃ³n de memoria: %d", punt_nro);
+    
+	struct empleados empl;
+	    
+	fflush(stdin);	    //limpia buffer
+	printf("\n\nIngrese Apellido del empleado: ");
+	fgets(empl.apellido, 10, stdin);
+	
+	fflush(stdin);	
+	printf("Ingrese nombre del empleado: ");
+	fgets(empl.nombre, 10, stdin);
+	
+	printf("Ingrese edad del empleado: ");
+	scanf("%d",&empl.edad);
+
+	printf("Ingrese sueldo del empleado: ");
+	scanf("%f",&empl.sueldo);
+
+	muestraDatos(&empl); //se hace el envío de la dirección de memoria para que no se tenga que hacer copia de la estructura completa.
 
     return 0;
+}
+
+
+void muestraDatos(struct empleados *emp){
+	printf("El empleado %s, %s con edad %d, tiene el siguiente sueldo: %.2f.", 
+			emp->apellido, emp->nombre, emp->edad, emp->sueldo); //Al ser puntero se reemplaza . por ->
 }
